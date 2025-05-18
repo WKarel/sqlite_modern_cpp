1cec6f5dac10880b593d57b31cfd928ca6622792
Wilfried Karel <wilfried.karel@geo.tuwien.ac.at>
2025-05-18 23:51:45 +0200

    VC 2022

--- hdr/sqlite_modern_cpp.h
+++ hdr/sqlite_modern_cpp.h
@@ -31,7 +31,7 @@ namespace sqlite {
 		using Index = std::conditional_t<Name, const char *, int>;
 
 		index_binding_helper(const index_binding_helper &) = delete;
-#if __cplusplus < 201703 || _MSVC_LANG && _MSVC_LANG <= 201703
+#if __cplusplus < 201703 || _MSVC_LANG // && _MSVC_LANG <= 201703
 		index_binding_helper(index_binding_helper &&) = default;
 #endif
 		index_binding_helper(Index index_, T value_) :
